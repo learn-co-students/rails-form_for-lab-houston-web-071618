e03e4fee06b782ebba0224eb0893207c30cef585
ELehotzky <Lehotzky.Erica@gmail.com>
2018-08-14 09:17:31 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+microbio = SchoolClass.create(title: "Microbiology", room_number: 304)
+physics = SchoolClass.create(title: "Physics", room_number: 102)
+epi = SchoolClass.create(title: "Epidemiology", room_number: 308)
+
+
+erica = Student.create(first_name: "Erica", last_name: "Lehotzky")
+joseph = Student.create(first_name: "Joseph", last_name: "Lehotzky")
+andrew = Student.create(first_name: "Andrew", last_name: "Lehotzky")
