896493591f5e8909c8b680de1a825bb362a7a398
github username <github email address>
2018-08-31 18:55:34 -0500

    create some shit

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,6 +4,8 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = SchoolClass.create(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
   end
 
   def show
