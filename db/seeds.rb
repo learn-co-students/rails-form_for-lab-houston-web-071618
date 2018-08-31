af5b35612339469db86ffc70615ce82c6396a377
github username <github email address>
2018-08-31 18:40:54 -0500

    Got the new form for school classes, plus a whooollle lotta other shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.all.destroy_all
+20.times do
+    Student.create(first_name: RandomWord.adjs.next, last_name:RandomWord.nouns.next)
+end
+
+SchoolClass.all.destroy_all
+8.times do 
+    SchoolClass.create(title: "#{RandomWord.phrases.next} #{Random.rand(999)}", room_number: Random.rand(5000))
+end
